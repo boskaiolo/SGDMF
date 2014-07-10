1470ec62a7250d07a41d4a166b69551e5936d233
Alberto Boschetti <dreambosko@gmail.com>
2014-07-10 21:41:49 +0100

    Sparse Matrix model and Data Importer

---
+++ Importer.py
@@ -0,0 +1,49 @@
+import urllib2
+import zipfile
+import os
+
+
+class DatasetImporter:
+    """
+    Import the dataset, and prepare it as a coo_matrix, i.e. a list of tuples [(row, col, val), ...]
+    """
+
+    zip_file = '/tmp/ml-100k.zip'
+    data_file = '/tmp/ml-100k/u.data'
+
+    def __init__(self):
+        if not self.check_if_file_exists():
+            print "Downloading the dataset"
+            self.download_and_unzip()
+
+        print "Parsing the dataset"
+        self.dataset = list(self.read_lines(self.data_file))
+
+    def download_and_unzip(self):
+        response = urllib2.urlopen('http://files.grouplens.org/datasets/movielens/ml-100k.zip')
+
+        with open(self.zip_file, 'w') as fh:
+            fh.write(response.read())
+
+        with zipfile.ZipFile(self.zip_file) as zfh:
+            zfh.extractall('/tmp/')
+
+    def check_if_file_exists(self):
+        if os.path.isfile(self.data_file) and os.access(self.data_file, os.R_OK):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def read_lines(data_file):
+        with open(data_file, "r") as fh:
+            lines = fh.readlines()
+
+        for line in lines:
+            chunk = line.split("\t")
+            yield int(chunk[0]), int(chunk[1]), float(chunk[2])
+
+
+
+di = DatasetImporter()
+print(di.dataset[0])
---
+++ SparseMatrix.py
@@ -0,0 +1,69 @@
+
+class SparseMatrix:
+    """
+    Sparse matrix with the capability of returning filled cells in rows and columns
+
+    Example:
+
+        [1    ]
+        [  2  ]
+    M = [  3  ]
+        [    4]
+        [5   6]
+
+    [Construct the matrix M]
+    M.getRow(4) = {0: 5, 2: 6} (that's the sparse representation of the first row
+    M.getCol(1) = {1: 2, 2: 3} (that's the sparse representation of the second column)
+
+    """
+
+    def __init__(self):
+        self.csr = {}
+        self.csc = {}
+
+    def addValue(self, i, j, val):
+        assert(type(i) == int and type(j) == int)
+        try:
+            self.csr[i][j] = val
+        except KeyError:
+            self.csr[i] = {}
+            self.csr[i][j] = val
+
+        try:
+            self.csc[j][i] = val
+        except KeyError:
+            self.csc[j] = {}
+            self.csc[j][i] = val
+
+    def addValues(self, list_of_elements):
+        assert (type(list_of_elements) == list)
+        for element in list_of_elements:
+            assert(type(element) == tuple)
+            self.addValue(element[0], element[1], element[2])
+
+    def getRow(self, row):
+        return self.csr.get(row, {})
+
+    def getCol(self, col):
+        return self.csc.get(col, {})
+
+    def shape(self):
+        return max(self.csr.keys())+1, max(self.csc.keys())+1
+
+
+if __name__ == "__main__":
+    matrix = SparseMatrix()
+    matrix.addValue(0, 0, 1.0)
+    matrix.addValue(1, 1, 2.0)
+    matrix.addValue(2, 1, 3.0)
+    matrix.addValue(3, 2, 4.0)
+    matrix.addValue(4, 0, 5.0)
+    matrix.addValue(4, 2, 6.0)
+    print(matrix.getRow(4))
+    print(matrix.getCol(1))
+    print(matrix.shape())
+
+    matrix.addValues([(10, 10, 10.0)])
+    print(matrix.getRow(6))
+    print(matrix.getCol(6))
+    print(matrix.shape())
